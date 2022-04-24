0c7ca0cb6e31a26ed5d9eb0d6b1533dcf65c8477
Digital Fusion <df1317@gmail.com>
2022-04-23 21:01:29 -0400

    Final code for 2022

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -2,6 +2,8 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
+// final code
+
 /*
  * !! Make sure hood is correctly positioned before powering on/off !!
  */
